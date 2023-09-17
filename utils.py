1e70889eb109f5c965057948ed71c62d5e560fbe
shellbertt <kaaden@ualberta.ca>
2023-09-16 21:35:02 -0600

    reset first message on new day

--- utils.py
+++ utils.py
@@ -1,6 +1,18 @@
+import os
 import os.path
+from datetime import datetime
 
 FIRST_LOCK = "./first_msg.txt"
 
-async def first_msg_exists():
+def first_msg_exists():
     return os.path.exists(FIRST_LOCK)
+
+async def reset_old_first():
+    if not first_msg_exists():
+        return
+   
+   cur_day = datetime.today().date()
+   last_claimed_day = date.fromtimestamp(os.path.getctime(FIRST_LOCK)).date()
+   if cur_day > last_claimed_day:
+       os.remove(FIRST_LOCK)
+
